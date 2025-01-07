048c74655c8b6dff4b461320ceb578ff97303b71
rahul <rahulra16809999@gmail.com>
2025-01-07 10:09:46 +0530

    Add state management and event handling in Home component
    - Implement useState to manage name state
    - Create apple function to update name state
    - Add InnerComp component within Home component
    - Add button to trigger apple function and update state
    - Render name state in JSX

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,66 @@
-import Image from "next/image";
+'use client' //Directive Definition for using client side javascript 
 import styles from "./page.module.css";
+import { useState } from "react";
 
 export default function Home() {
+  const [name, setName] = useState("Rahul");
+
+  // let data = "Rahul";
+
+  // const apple = (item) =>{
+  //   alert(item);
+  // }
+
+  // USING STATE DATA
+  const apple = () => {
+    setName("John");
+  }
+
+  // USING VARIABLE DATA
+  // const apple = () =>{
+  //   data = "John";
+  // }
+
+  // COMPONENT UNDER COMPONENT
+  const InnerComp = () => {
+    return(
+      <div>
+        <h3>Inner Component</h3>
+      </div>
+    );
+  }
+
   return (
     <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+      {/* <User name="Rahul"/>
+      <User name="John"/>
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      <h1> Home Page </h1>
+
+      <User name="Sam"/>
+      <User name="Alice"/> */}
+
+      <h1>Events, function and state {name}</h1>
+      {/* <h1>Events, function and state {data}</h1> */}
+      {/* <button onClick={() => alert("Hello Rahul!")}>Click Me</button> */}
+      {/* <button onClick={() => apple("color")}>Click Me</button> */}
+      <button onClick={ () => apple()}>Click Me</button>
+      
+      {/* CALLING INNER COMPONENT */}
+      {/* <InnerComp /> */}
+
+      {/* CALL INNER COMPONENTS THROUGH JSX FORMAT MEANS NORMAL FUNCTION */}
+      {InnerComp()}
+
     </div>
   );
 }
+
+
+// const User = (props) => {
+//   return (
+//     <div>
+//       <h2>Hii Duniya! This is {props.name}</h2>
+//     </div>
+//   );
+// }
