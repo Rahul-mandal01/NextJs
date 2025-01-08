31ccdb49fe71bbf7ca5a13adbad1099b47f985a5
rahul <rahulra16809999@gmail.com>
2025-01-08 11:06:11 +0530

    feat: add navigation buttons and links to Home component
    - Added a navigate function to handle routing
    - Added links to Login and About pages
    - Updated the Home component with basic routing functionality

---
+++ src/app/login/page.js
@@ -0,0 +1,11 @@
+import Link from "next/link"
+const Login = () => {
+    return(
+        <div>
+            <h1>Login Page</h1>
+            <Link href="/">Go to Home Page</Link>
+        </div>
+    );
+}
+
+export default Login;
