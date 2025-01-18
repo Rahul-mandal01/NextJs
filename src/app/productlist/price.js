4d7f8e9ac2a5e12578f4be1160f46e0414ee3303
rahul <rahulra16809999@gmail.com>
2025-01-18 21:57:59 +0530

    Deleted Unnecessary files

--- src/app/productlist/price.js
+++
@@ -1,9 +0,0 @@
-"use client"
-export default function Price(props){
-    console.log(props);
-    return(
-        <div>
-            <button onClick={() => alert(props.price)}>Check Price</button>
-        </div>
-    );
-}
