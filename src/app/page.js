ef3aea0ab1ec763f8b647150627295aee7421092
rahul <rahulra16809999@gmail.com>
2025-01-20 19:27:35 +0530

    Implement module CSS

--- src/app/page.js
+++ src/app/page.js
@@ -1,17 +1,17 @@
 "use client";
-import Link from "next/link";
-import styles from "./page.module.css";
+import custom from './custom.module.css';
+import other from './other.module.css';
+import outside from '@/style/outside.module.css';
 
 
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <div>
-        <h1> Style and CSS with Next.js </h1>
-        <h2> Heading 2 in main page </h2>
-      </div>
-    </div>
-  );
+    <main>
+      <h1 className={custom.main}>CSS Modules with Next js</h1>
+      <h2 className={other.main}>Heading 2 in main page</h2>
+      <h2 className={outside.main}>Outside CSS</h2>
+    </main>
+  ); 
 }
 
