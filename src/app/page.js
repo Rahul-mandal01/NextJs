dd12fdff6992bd116f3c70460fe35a2abdf5ac4f
rahul <rahulra16809999@gmail.com>
2025-01-21 20:41:45 +0530

    Add image optimization example in Home component
    - Added an example of image optimization using the next/image component.
    - Included a sample image with specified width and height.
    - Logged the Profile image to the console.

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,16 @@
+import Image from "next/image";
+import Profile from "../../public/next.svg";
 export default function Home() {
+  console.log(Profile);
   return (
     <main>
-
+      <h1>Image Optimization in Next js</h1>
+      {/* <Image src={Profile} />
+      <img src={Profile.src} /> */}
+      <Image 
+      src="https://as2.ftcdn.net/v2/jpg/07/23/14/93/1000_F_723149335_tA0Fo8zefrHzYlSgXRMYHmBQk7CuWrRd.jpg" 
+      width={700} height={500} alt=""
+      />
     </main>
   );
 }
