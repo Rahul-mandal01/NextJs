c8d84477044835c4a5af7309d3f43d71c0a787d6
rahul <rahulra16809999@gmail.com>
2025-01-21 20:48:00 +0530

    Delete image optimization code

--- src/app/page.js
+++ src/app/page.js
@@ -4,13 +4,7 @@ export default function Home() {
   console.log(Profile);
   return (
     <main>
-      <h1>Image Optimization in Next js</h1>
-      {/* <Image src={Profile} />
-      <img src={Profile.src} /> */}
-      <Image 
-      src="https://as2.ftcdn.net/v2/jpg/07/23/14/93/1000_F_723149335_tA0Fo8zefrHzYlSgXRMYHmBQk7CuWrRd.jpg" 
-      width={700} height={500} alt=""
-      />
+
     </main>
   );
 }
