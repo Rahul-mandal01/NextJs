71c8fa3eed81ea901e73f0e9b35e3e462953ab82
rahul <rahulra16809999@gmail.com>
2025-01-21 21:53:28 +0530

    Add Roboto font optimization in Home component
    - Integrated Roboto font using next/font/google.
    - Applied Roboto font with specific weight and subsets.
    - Displayed headings with the optimized Roboto font.

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,15 @@
-import Image from "next/image";
-import Profile from "../../public/next.svg";
+import { Roboto } from "next/font/google";
+const roboto = Roboto({
+  weight:'100',
+  subsets:['latin'],
+  display:'swap'
+})
 export default function Home() {
-  console.log(Profile);
   return (
     <main>
-
+      <h1>Font Optimization in Next js</h1>
+      {/* <h1 style={{fontFamily:'Roboto', fontWeight:200}} >Font with Link Tag in Next</h1> */}
+      <h1 className={roboto.className}> Font with Next font feature</h1>
     </main>
   );
 }
