29c776cab64e3b5d922fd61c5ec9caaaaa972a02
rahul <rahulra16809999@gmail.com>
2025-01-25 21:13:32 +0530

    Delete Loader logic

--- src/app/users/loading.js
+++
@@ -1,9 +0,0 @@
-
-import './loading.css'
-export default function Page(){
-    return(
-        <div className="custom-loader">
-           {/* <h1>Loading user list...</h1>  */}
-        </div>
-    )
-}
