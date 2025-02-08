20f507fd4cca463b506514ec5d3c521fd0566874
rahul <rahulra16809999@gmail.com>
2025-02-08 21:45:14 +0530

    feat: add environment variable display in Home component
    - Implemented Home component
    - Added conditional rendering based on NODE_ENV
    - Displayed environment variables DB_PASSWORD, SERVER_PASSWORD, CUSTOM_MODE, and API_BASE_URL

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,19 @@
+import { API_BASE_URL } from "@/config/constants";
+
 export default function Home() {
+  console.log(process.env.DB_PASSWORD);
+  console.log(process.env.SERVER_PASSWORD);
+  console.log(process.env.CUSTOM_MODE);
   return (
     <main>
-      <h1>Redirection in Next.js</h1>
+      {process.env.NODE_ENV == "development" ? (
+        <h1>You are on Development Mode</h1>
+      ) : (
+        <h1>You are on Production Mode</h1>
+      )}
+      <h1>Environmet variable in Next.js</h1>
+
+      <h1>{API_BASE_URL}</h1>
     </main>
   );
 }
